0f036dd134e9fa0a2b1141707cf4c626e6384c96
fisayoomomogbe <doctoromo@yahoo.com>
2021-05-04 20:24:49 -0600

    Created views and layouts

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const express = require("express");
 const userRouter = express.Router();
 
 // Login router
-userRouter.get("/login", (req, res) => res.send("Login"));
+userRouter.get("/login", (req, res) => res.render("Login"));
 
 // Register router
 
-userRouter.get("/register", (req, res) => res.send("Register"));
+userRouter.get("/signup", (req, res) => res.render("Signup"));
 
 module.exports = userRouter;
