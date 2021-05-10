0dd94be7b192b9fd52d85e5981327311447c52d2
fisayoomomogbe <doctoromo@yahoo.com>
2021-05-10 10:27:42 -0600

    updated index route file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+const { ensureAuthenticated } = require("../config/auth");
+// Home page
 router.get("/", (req, res) => res.render("home"));
+// Dashboard page
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+  res.render("dashboard", { name: req.user.name })
+);
 
 module.exports = router;
